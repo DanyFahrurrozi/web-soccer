3f92b2c563c614510c36ce0d019f44380bc2cd49
DanyFahrurrozi <danyfahrurrozi69@gmail.com>
2021-11-16 14:48:06 -0800

    menambahkan service dan view di standings

---
+++ src/app/interface/ranking.ts
@@ -0,0 +1,8 @@
+export interface Ranking {
+    TeamName: String;
+    GamePlayed: number;
+    Wins: number;
+    Ties: number;
+    GoalsFor: number;
+    GoalsAgaints: number;
+}
---
+++ src/app/service/ScheduleData.ts
@@ -0,0 +1,29 @@
+import { Schedule } from "../interface/schedule";
+import { Team } from "../interface/team";
+
+export const SEASON_SCHEDULE:Schedule[]=[
+    {id:1,PlayingDate:new Date(2021,08,10),HomeTeam:'Persija',
+    AwayTeam:'Persipon',HomeScore:3,AwayScore:2,
+    RefName:'Joko',Notes:'Overtime'},
+    {id:2,PlayingDate:new Date(2021,08,12),HomeTeam:'RRQ',
+    AwayTeam:'Persipon',HomeScore:3,AwayScore:2,
+    RefName:'Budi',Notes:'Overtime'},
+    {id:3,PlayingDate:new Date(2021,08,12),HomeTeam:'RRQ',
+    AwayTeam:'Bali United',HomeScore:3,AwayScore:2,
+    RefName:'Bambang',Notes:'Overkill'},
+    {id:4,PlayingDate:new Date(2021,08,10),HomeTeam:'EVOS',
+    AwayTeam:'Black Internasional',HomeScore:3,AwayScore:2,
+    RefName:'Budi',Notes:'Overtime'},
+    {id:5,PlayingDate:new Date(2021,08,10),HomeTeam:'Navi',
+    AwayTeam:'Todak',HomeScore:3,AwayScore:2,
+    RefName:'Budi',Notes:'Navi agak lag nich'},
+]
+export const Teams:Team[]=[
+    {id:1,name:'Persija',type:'Klub lokal'},
+    {id:2,name:'Persipon',type:'Klub lokal'},
+    {id:3,name:'Bali United',type:'Klub lokal'},
+    {id:4,name:'Evos',type:'Klub internasional'},
+    {id:5,name:'RRQ',type:'Klub internasional'},
+    {id:6,name:'Todak',type:'Klub lokal'},
+    {id:7,name:'Navi',type:'Klub lokal'},
+]
